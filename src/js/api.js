8deb3937b954877a0054e365c119314b5958bc1f
SeRgNeschetniy <xdsergx@gmail.com>
2022-08-25 11:16:21 +0300

    add storage

--- src/js/api.js
+++ src/js/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export class themoviedbAPI {
   #API_KEY = 'f2c538717486cd7dc01f7314dfb697ab';
   #BASE_URL = 'https://api.themoviedb.org/3';
+  #page;
 
   async getPopularMovies() {
     const url = `${this.#BASE_URL}/trending/movie/week?api_key=${
