78016f525839e54ddc69f5de033914c1c81b4b7a
Alex Makeyev <makeyev1987@gmail.com>
2022-08-28 00:11:11 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { } from './js/preloader'
-
+import './js/auth'; // * authentification
 import './js/team-modal'; // * скріпт модалки про команду
 import './js/_sing-in-up-modal' // * скрипт на відкриття модалки для реєстрації
 import { themoviedbAPI } from './js/api/API';
