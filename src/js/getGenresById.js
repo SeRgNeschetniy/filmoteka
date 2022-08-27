541eaeac3b886afe0fdf242a3fbbc22fc954c715
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-08-27 08:33:16 +0300

    add function getGenresById

---
+++ src/js/getGenresById.js
@@ -0,0 +1,16 @@
+import { load, GENREFILMS_LOCALSTORAGE_KEY } from './storage/storage';
+
+export function getGenresById(genre_ids) {
+  if (genre_ids.length !== 0) {
+    const allGenres = load(GENREFILMS_LOCALSTORAGE_KEY);
+
+    const myGenres = allGenres.filter(genre => genre_ids.includes(genre.id));
+
+    return myGenres.length > 3
+      ? myGenres
+          .slice(0, 2)
+          .map(genre => genre.name)
+          .join(', ') + ', Other'
+      : myGenres.map(genre => genre.name).join(', ');
+  } else return 'Genres is not found';
+}
