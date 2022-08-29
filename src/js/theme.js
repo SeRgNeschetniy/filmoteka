cadae59b994c37bafc4d13f09e72060d2fc12727
SeRgNeschetniy <xdsergx@gmail.com>
2022-08-29 15:04:40 +0300

    bugfix/theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -6,19 +6,25 @@ const refs = {
   tumbler: document.querySelector('.tumbler-wrapper'),
 };
 
-const theme = load('THEME_LOCALSTORAGE_KEY')
-  ? load('THEME_LOCALSTORAGE_KEY')
-  : '';
-document.body.classList.add('theme');
+const theme = load('THEME_LOCALSTORAGE_KEY');
+document.body.classList.add(theme);
 
 if (refs.tumbler) {
   refs.tumbler.addEventListener('click', e => {
-    if (theme === '') {
+    const theme = load('THEME_LOCALSTORAGE_KEY');
+
+    console.log('themeload', theme);
+    if (theme === 'light-mode') {
+      console.log('theme1', theme);
+      document.body.classList.remove('light-mode');
+      document.body.classList.add('night-mode');
       save(THEME_LOCALSTORAGE_KEY, 'night-mode');
     } else {
-      save(THEME_LOCALSTORAGE_KEY, '');
+      console.log('theme2', theme);
+      document.body.classList.remove('night-mode');
+      document.body.classList.add('light-mode');
+      save(THEME_LOCALSTORAGE_KEY, 'light-mode');
     }
-    save(THEME_LOCALSTORAGE_KEY, theme);
-    document.body.classList.toggle('night-mode');
+    //document.body.classList.toggle('night-mode');
   });
 }
