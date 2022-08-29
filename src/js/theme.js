c5fce80f36bfb32718555393c25521aa224ba9ff
SeRgNeschetniy <xdsergx@gmail.com>
2022-08-29 12:48:43 +0300

    comit

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,9 +1,24 @@
+import { save, load } from './storage/storage';
+
+const THEME_LOCALSTORAGE_KEY = 'theme';
+
 const refs = {
   tumbler: document.querySelector('.tumbler-wrapper'),
 };
 
+const theme = load('THEME_LOCALSTORAGE_KEY')
+  ? load('THEME_LOCALSTORAGE_KEY')
+  : '';
+document.body.classList.add('theme');
+
 if (refs.tumbler) {
   refs.tumbler.addEventListener('click', e => {
+    if (theme === '') {
+      save(THEME_LOCALSTORAGE_KEY, 'night-mode');
+    } else {
+      save(THEME_LOCALSTORAGE_KEY, '');
+    }
+    save(THEME_LOCALSTORAGE_KEY, theme);
     document.body.classList.toggle('night-mode');
   });
 }
