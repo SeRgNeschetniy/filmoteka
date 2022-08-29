49842ca960fc20e5268834851aa4d9faa6bdfd64
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-08-29 17:12:01 +0300

    popup styles done

--- src/js/popup.js
+++ src/js/popup.js
@@ -33,49 +33,53 @@ const modalMoviemarkup = ({
   adult,
 }) => {
   const genresNames = getAllGenresForModal(genre_ids);
-  return `<div class="popup__btn--close js-close-btn">
-          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-x-lg"
-            viewBox="0 0 16 16">
-            <path
-              d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-          </svg>
-        </div>
-        <div class="popup__img-wrapper">
-          <img class="popup__img" src="https://image.tmdb.org/t/p/w400/${poster_path}" alt="${title}" />
-        </div>
-        
-        <div class="popup__info-container film-info">
-          <h2 class="film-info__title">${original_title}</h2>
-          <ul class="film-info__details">
-            <li class="film-info__item">
-              <span class="film-info__category">Vote / Votes</span>
-              <span class="film-info__value">
-                <span class="film-info__value--vote">${vote_average}</span> / <span
-                  class="film-info__value--votes">${vote_count}</span>
-              </span>
-            </li>
-            <li class="film-info__item">
-              <span class="film-info__category">Popularity</span>
-              <span class="film-info__value">${popularity}</span>
-            </li>
-            <li class="film-info__item">
-              <span class="film-info__category">Original Title</span>
-              <span class="film-info__value">${original_title}</span>
-            </li>
-            <li class="film-info__item">
-              <span class="film-info__category">Genre</span>
-              <span class="film-info__value">${genresNames}</span>
-            </li>
-          </ul>
-          <h3 class="film-info__about-title">About</h3>
-          <p class="film-info__about-text">
-            ${overview}
-          </p>
-          <div class="popup__btn">
-            <button class="popup__btn--watched" type="button" data-id=${id}>add to Watched</button>
-            <button class="popup__btn--queue" type="button" data-id=${id}>add to queue</button>
-          </div>
-        </div>
+  return `<button class="popup__btn--close js-close-btn" type="button" data-modal-close>
+  <svg class="popup__svg--close" viewBox="0 0 30 30" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path d="m8 8 14 14M8 22 22 8" stroke="#000" stroke-width="2" />
+  </svg>
+</button>
+<div class="popup__img-wrapper">
+  ${
+    poster_path === null
+      ? `<img src="${placeholderImg}" alt="${title}" class="popup__img">`
+      : `<img src="https://image.tmdb.org/t/p/w400/${poster_path}" alt="${title}" class="popup__img">`
+  }
+</div>
+<div class="popup__info-container film-info">
+  <h2 class="film-info__title">${title}</h2>
+  <ul class="film-info__details-list">
+  <li class="film-info__details-item">
+    <p class="film-info__category">Vote / Votes</p>
+    <p class="film-info__value">
+      <span class="film-info__value--vote">${
+        vote_average ? vote_average.toFixed(1) : '-'
+      }</span> / <span class="film-info__value--votes">${
+    vote_count ? vote_count : '-'
+  }</span>
+    </p>
+  </li>
+  <li class="film-info__details-item">
+    <p class="film-info__category">Popularity</p>
+    <p class="film-info__value">${popularity ? popularity : '-'}</p>
+  </li>
+  <li class="film-info__details-item">
+    <p class="film-info__category">Original Title</p>
+    <p class="film-info__value">${original_title ? original_title : title}</p>
+  </li>
+  <li class="film-info__details-item">
+    <p class="film-info__category">Genre</p>
+    <p class="film-info__value">${genresNames}</p>
+  </li>
+</ul>
+  <h3 class="film-info__about-title">About</h3>
+  <p class="film-info__about-text">
+    ${overview}
+  </p>
+  <div class="popup__btn-container">
+    <button class="popup__btn" type="button" data-id=${id}>add to Watched</button>
+    <button class="popup__btn" type="button" data-id=${id}>add to queue</button>
+  </div>
+</div>
   `;
 };
 
@@ -85,14 +89,14 @@ if (refs.moviesList) {
 
     popupHandler(e.target);
     refs.close = document.querySelector('.js-close-btn');
-    refs.popup.classList.add('is-wisible');
+    refs.popup.classList.toggle('is-hidden');
     window.addEventListener('keydown', escapeClose);
     refs.close.addEventListener('click', closePopup);
   });
 }
 
 function closePopup() {
-  refs.popup.classList.remove('is-wisible');
+  refs.popup.classList.toggle('is-hidden');
   window.removeEventListener('keydown', escapeClose);
 }
 
